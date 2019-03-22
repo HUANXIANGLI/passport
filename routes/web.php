7e82f9f3fbdc282c26222eae484087e17ae0b30b
liruixiang <3077546454@qq.com>
2019-03-22 16:08:05 +0800

    api

--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,7 @@ Route::get('/', function () {
 //登录
 Route::post('login','Index\IndexController@login');
 Route::get('login','Index\IndexController@loginView');
-Route::any('apiLogin','Index\IndexController@apiLogin');
+Route::post('apiLogin','Index\IndexController@apiLogin');
 
 //注册
 Route::post('register','Index\IndexController@register');
